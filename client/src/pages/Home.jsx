faae85b28a26ef05c84a68cdb575fafcc0bd01f3
Fik0u <fikouch@gmail.com>
2025-05-01 11:37:16 +0100

    Home Page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,18 +6,16 @@ import ProdsList from '../components/ProdsList';
 const Home = () => {
   const dispatch = useDispatch();
   const products = useSelector(state => state.prodReducer.products)
-  // console.log(products)
+  
   useEffect(() => {
     dispatch(getAllProds())
   }, [dispatch])
 
   return (
-    <div>
+    <div style={{ backgroundColor: '#f5f5f5', minHeight: '100vh' }}>
+      <img src="https://img.lovepik.com/background/20211022/large/lovepik-digital-high-tech-picture-background-image_500586019.jpg" alt="home" style={{width: '100%', height: '400px'}} />
 
-        <img src="https://t3.ftcdn.net/jpg/06/03/27/60/360_F_603276027_JqJCl11RdIw0flpvv2vuCDkYgs0xxtv7.jpg" alt="home" style={{width: '100%', height: '500px'}} />
-
-        <ProdsList products = {products} all = {true} />
-
+      <ProdsList products={products} all={true} />
     </div>
   )
 }
